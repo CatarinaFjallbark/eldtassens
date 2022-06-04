744e89ba30c62e44e5e712afa1454ebab2cc29a5
Catarina Sörensen <catarina.sorensen@jayway.com>
2022-06-04 21:42:35 +0200

    Updates presentation page

--- src/Pages/Presentation.js
+++ src/Pages/Presentation.js
@@ -10,13 +10,15 @@ Mitt mål är att ta fram allroundhundar som med bra inställning följer övera
 
 <h1>VEM ÄR JAG?</h1>
 <p>Jag har älskat djur så länge som jag kan minnas, redan ett par år gammal drömde jag om en egen gård och egna hundar. Under uppväxten hade vi en Labrador Retriever, efter ett par år med hund-paus när jag studerade och började mitt yrkesliv så funderade jag länge på vilken ras som skulle passa mig. Jag letade efter en frisk och aktiv hundras, i mellanstorlek som också är trevlig att ha att göra med. Det landade på Finsk Lapphund och jag har inte ångrat mig en sekund.
-
+<br />
+<br />
 Jag visste att jag ville ha en tik men det primära målet var absolut ej avel utan jag ville ha en hund till träning och sällskap. Planen var att träna Agility men både min första finska lapphund och jag tyckte rallylydnad var roligast så det har vi tränat en del. När min första Finska lapphund var omkring ett år började min barndomsdröm att föda upp en valpkull väckas till liv så smått och jag visste att om det någonsin skulle realiseras så gällde det att lära sig så mycket som möjligt om hundar, avel och uppfödning. Även ta reda på om min tik är tillräckligt bra för att användas i avel och har något att tillföra till rasen.
-
+<br />
+<br />
 Jag anmälde mig till SKKs uppfödarkurs samt fortsättningskursen om genetik och såg till att prata med många etablerade uppfödare för att få så mycket kunskap som möjligt. 2021 var året jag fick min första valpkull, efter fina hälsotester och hjälp från min tiks uppfödare att hitta den perfekta hanen så kom en fantastisk kull under sommaren. Jag la ner min själ i att hitta fantastiska valpköpare till alla valpar och valde efter många om och men att inte behålla någon valp själv. Under hösten funderade jag mycket på om detta var något jag ville göra fler gånger och det kom jag fram till att jag ville så jag bestämde mig för att lära mig ännu mer om rasen, exempelvis förstå rasstandarden, lära mig om olika linjer och knyta kontakter.
-
+<br />
+<br />
 Mina hundar är familjemedlemmar och denna kenneln kommer föda upp enstaka kullar men alla i hemmamiljö. När jag inte myser eller tränar med mina hundar så jobbar jag som konsult inom webbutveckling i Malmö.
-
 Jag och min sambo Johan bor i ett radhus precis utanför Malmö med i dagsläget tre hundar hemma.</p>
 
 <h1>FINSK LAPPHUND</h1>
