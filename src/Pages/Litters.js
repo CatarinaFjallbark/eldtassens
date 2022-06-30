079ce24410ce4237e33de8ca893d3378e3eb1dca
Catarina Sörensen <catarina.sorensen@jayway.com>
2022-06-30 20:00:11 +0200

    Adds images

--- src/Pages/Litters.js
+++ src/Pages/Litters.js
@@ -3,12 +3,21 @@ import React from 'react'
 const Litters = () => <div className="Container">
     <h1>B-kullen</h1>
     <h3>Födda 2022-06-01, leveransklara omkring månadsskiftet till Augusti</h3>
-    <p>4 hanar och 1 tik, seriösa intressenter är välkomna att maila. Samtliga svarta med ljusa tecken, stora härliga valpar, se Instagram för bilder.</p>
+    <p>4 hanar och 1 tik, seriösa intressenter är välkomna att maila. Svarta med ljusa tecken, en blir eventuellt viltfärgad, stora härliga valpar, se Instagram för bilder. Alla till salu, en hane säljes för halva priset där jag behåller avelsrätten.</p>
+    <img src={require('../assets/gnistror.jpeg')} alt='Gnistra kull'></img>
+    <img src={require('../assets/blitz.jpeg')} alt='Gnistra kull'></img>
     <img src={require('../assets/gnistraLegoWeb.png')} alt='Gnistra kull'></img>
+    <hr />
     <h1>C-kullen</h1>
     <h3>Födda 2022-06-05, leveransklara omkring månadsskiftet till Augusti</h3>
-    <p>4 hanar och 1 tik, seriösa intressenter är välkomna att maila. Två creme, två bruna, en svart, samtliga med ljusa tecken, se Instagram för bilder.</p>
+    <p>3 hanar och 1 tik, seriösa intressenter är välkomna att maila. En creme, två bruna (eventuellt viltfärgade), en svart (eventuellt vilt eller dominofärgad), samtliga med ljusa tecken, se Instagram för bilder. Hanarna till salu, någon hane säljes för halva priset där jag behåller avelsrätten.</p>
+    <img src={require('../assets/askas.jpeg')} alt='Gnistra kull'></img>
     <img src={require('../assets/askaAckoWeb.png')} alt='Aska kull'></img>
+    <hr />
+    <h1>A-kullen</h1>
+    <h3>Födda 2021-07-20</h3>
+    <p>2 hanar och 3 tikar. Samtliga svarta med ljusa tecken, se Instagram för bilder.</p>
+    <p>Black Track’s Gnistra - Lapprots Texas Fudge</p>
 </div>
 
 export default Litters;
